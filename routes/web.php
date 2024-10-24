12d340f9d67990b3a8db394f6c7d53ca28fd00ba
Vitaliy <kv1993@me.com>
2024-10-24 13:22:01 +0700

    Added route test_database which test database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use App\Http\Controllers\FormProcessor;
+use App\Models\Employee;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/userform', [FormProcessor::class, 'index']);
 Route::post('/store_form', [FormProcessor::class, 'store']);
+
+Route::get('/test_database', function () {
+    $employee = new Employee();
+    $employee->save();
+});
+
